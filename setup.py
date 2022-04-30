8c93c6d73442e375f06a75129af0bc9fbffbd81a
織戸悠輔 <oritoyusuke@oridoyuusukenoMacBook-Air.local>
2022-04-30 19:43:41 +0900

    update setup

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+from grpc import composite_channel_credentials
 import setuptools
+
+PYTHON_REQUIRES = ">=3.7"
+
+
+setuptools.setup(
+   name= "yusuke_library",
+   author="yusuke orito",
+   description="my library for competition",
+   version="0.1.0",
+   url="https://github.com/yusukeorito/my_lib.git",
+   python_requires=PYTHON_REQUIRES,
+
+)
