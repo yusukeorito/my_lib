acda84a28da8d1fc95bd2d0306f2213daaac3135
織戸悠輔 <oritoyusuke@oridoyuusukenoMacBook-Air.local>
2022-05-01 17:45:44 +0900

    update setup

--- setup.py
+++ setup.py
@@ -2,6 +2,17 @@ from grpc import composite_channel_credentials
 import setuptools
 
 PYTHON_REQUIRES = ">=3.7"
+INSTALL_REQUIRES = [
+    "scikit-learn = 1.0.2",
+    "plotly = ^5.5.0",
+    "tqdm = ^4.62.3",
+    "python-box = ^5.4.1",
+    "ttach = ^0.0.3",
+    "category-encoders = ^2.3.0",
+    "lightgbm = ^3.3.2",
+    "xgboost = ^1.5.2",
+]
+
 
 
 setuptools.setup(
